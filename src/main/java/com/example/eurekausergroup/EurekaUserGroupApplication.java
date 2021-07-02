fb083a1f946263e91647ff55faa86e67e6257080
ambosing <ambosing_@naver.com>
2021-07-02 11:26:51 +0900

    UserGroup add

---
+++ src/main/java/com/example/eurekausergroup/EurekaUserGroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekausergroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaUserGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaUserGroupApplication.class, args);
+    }
+
+}
