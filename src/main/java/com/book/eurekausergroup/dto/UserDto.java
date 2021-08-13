ad2c8bdd544f3f7602609d7a62f37804cb7b176a
Yang Jiwon <ambosing_@naver.com>
2021-08-13 16:56:42 +0900

    PostgreSQL 연동 작업

--- src/main/java/com/book/eurekausergroup/dto/UserDto.java
+++ src/main/java/com/book/eurekausergroup/dto/UserDto.java
@@ -4,9 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserDto {
-    private String email;
     private String name;
-    private String userId;
-    private String eId;
-    // 북마크 게시글 관리..?
+    private String email;
+    private String uid;
 }
--- src/main/java/com/book/eurekausergroup/security/AuthenticationFilter.java
+++ src/main/java/com/book/eurekausergroup/security/AuthenticationFilter.java
@@ -72,13 +72,13 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         UserDto userDetails = userService.getUserDetailByEmail(userEmail);
 
         String token = Jwts.builder()
-                .setSubject(userDetails.getUserId())
+                .setSubject(userDetails.getUid())
                 .setExpiration(new Date(System.currentTimeMillis() +
                         Long.parseLong(env.getProperty("token.expiration_time"))))
                 .signWith(SignatureAlgorithm.HS512, env.getProperty("token.secret"))
                 .compact();
 
         response.addHeader("token", token);
-        response.addHeader("userId", userDetails.getUserId());
+        response.addHeader("userId", userDetails.getUid());
     }
 }
